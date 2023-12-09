fe2ec61600dbadad82f5070ad6bc2f0c8b785bd7
fixedfox <fixedfox@live.ru>
2023-12-09 23:12:07 +0300

    ДЗ от 02.12.2023 Реализовал GameHelper и тесты для него.

--- src/main/java/ru/fixedfox/thetwentyfourtyeightgame/GameHelper.java
+++ src/main/java/ru/fixedfox/thetwentyfourtyeightgame/GameHelper.java
@@ -4,11 +4,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameHelper {
-    List<Integer> moveAndMergeEqual(List<Integer> list){
-        var movedAndMergedItems = new ArrayList<Integer>();
-        for (Integer list1: list) {
+    public List<Integer> moveAndMergeEqual(List<Integer> list) {
+        var movedAndMergedItems = new ArrayList<>(list);
+        var listWithoutNull = leftShifter(movedAndMergedItems);
+        for (int j = 0; j < listWithoutNull.size() - 1; j++) {
 
+            var left = listWithoutNull.get(j);
+            var right = listWithoutNull.get(j + 1);
+
+            if (left == null) {
+                listWithoutNull.set(j, right);
+                listWithoutNull.set(j + 1, null);
+                listWithoutNull = leftShifter(listWithoutNull);
+                continue;
+            }
+            if (right == null) {
+                continue;
+            }
+
+            if (left.equals(right)) {
+                listWithoutNull.set(j, left * 2);
+                listWithoutNull.set(j + 1, null);
+                listWithoutNull = leftShifter(listWithoutNull);
+                continue;
+            }
         }
-        return movedAndMergedItems;
+        return listWithoutNull;
+    }
+
+    private List<Integer> leftShifter(List<Integer> list) {
+        var shiftedList = new ArrayList<>(list);
+        for (int i = 0; i < shiftedList.size() - 1; i++) {
+            var left = shiftedList.get(i);
+            var right = shiftedList.get(i + 1);
+
+            if (left != null) {
+                continue;
+            } else {
+                int j = i + 1;
+                while (j < (shiftedList.size())) {
+                    right = shiftedList.get(j);
+                    if (right != null) {
+                        shiftedList.set(i, right);
+                        shiftedList.set(j, null);
+                        break;
+                    }
+                    j++;
+                }
+            }
+        }
+        return shiftedList;
     }
 }
+
--- src/main/java/ru/fixedfox/thetwentyfourtyeightgame/Key.java
+++ src/main/java/ru/fixedfox/thetwentyfourtyeightgame/Key.java
@@ -1,6 +1,5 @@
 package ru.fixedfox.thetwentyfourtyeightgame;
 
-import java.util.Map;
 import java.util.Objects;
 
 public class Key {
